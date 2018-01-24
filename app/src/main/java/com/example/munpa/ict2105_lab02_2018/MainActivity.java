9bbc0152e1880c587cc3952f0f62fb8d7ba4a5fa
BigabuBeaze <munpan475@hotmail.com>
2018-01-24 13:46:51 +0800

    Updated with intent sending stuff from the tut... v2

--- app/src/main/java/com/example/munpa/ict2105_lab02_2018/MainActivity.java
+++ app/src/main/java/com/example/munpa/ict2105_lab02_2018/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.munpa.ict2105_lab02_2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**Called when the user taps the Send button */
+    public void sendMessage(View view){
+        //Do something in response to the button.
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
 }
